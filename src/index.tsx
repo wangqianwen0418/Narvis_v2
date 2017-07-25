085083fdfe5143757aace5adf1f196b01948f290
wangqianwen0418 <wangqianwen0418@163.com>
2017-07-25 23:26:19 +0800

    immigrate to material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,47 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Col, Row, Grid } from 'react-bootstrap';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import { GridList, GridTile } from 'material-ui/GridList';
 
 import { Title } from "./components/Title";
 import View from "./components/View";
 
-const Main = () => 
-<div>
-<Title title='NarVis'/>
-<Grid>
-    <Row>
-        <Col lg={4}>
-            <View heading='AA' body='' height={45}/>
-            <View heading='AA' body='' height={45}/>
-        </Col>
-        <Col lg={8}>
-            <View heading='AA' body='' height={70} />
-            <View heading='AA' body='' height={20} />
-        </Col>
-    </Row>    
-</Grid>
-</div>
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        display: 'flex',
+        flexWrap: 'nowrap',
+        overflowX: 'auto',
+    },
+    titleStyle: {
+        color: 'rgb(0, 188, 212)',
+    },
+};
+const Main = () =>
+    <MuiThemeProvider>
+        <GridList cols={12}>
+            <GridTile cols={2} rows={1}>
+                <Title title='NarVis' />
+            </GridTile>
+            <GridTile cols={2} rows={1}>
+                <View heading='AA' body='' height={45} />
+            </GridTile>
+            <GridTile cols={2} rows={1}>
+                <View heading='AA' body='' height={45} />
+            </GridTile>
+            <GridTile cols={2} rows={1}>
+                <View heading='AA' body='' height={70} />
+            </GridTile>
+            <GridTile cols={2} rows={1}>
+                <View heading='AA' body='' height={20} />
+            </GridTile>
+        </GridList>
+    </MuiThemeProvider>
 
 ReactDOM.render(
     <Main />,
